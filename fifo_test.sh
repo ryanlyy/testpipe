b875d9fbf5e29fbaeb13ec68dd66843006a923e1
skystone <ryanlyy@126.com>
2023-10-10 15:43:11 +0800

    Update fifo_test.sh

--- fifo_test.sh
+++ fifo_test.sh
@@ -4,7 +4,7 @@ while [[ $idx -le 2000 ]]; do
         idx=$idx+1
         FF_NAME=./fifo200-$idx
         echo "$FF_NAME"
-	./def $FF_NAME
+	./mk_fifo.sh $FF_NAME
 #        mkfifo ./fifo200-$idx
 #        python3 ./check.py ./fifo200-$idx &
 #        echo "testdata" > ./fifo200-$idx 
